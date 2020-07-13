e87c833bf58e4a2c50340b745051c82854630b10
Aditya Mudgerikar <aditya18thm@gmail.com>
2020-07-13 09:24:27 +0530

    AWS Amplify Setup

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -60,3 +60,10 @@ import 'zone.js/dist/zone'; // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
+
+//  AWS Amplify Setup
+(window as any).global = window;
+(window as any).process = {
+  env: { DEBUG: undefined },
+};
