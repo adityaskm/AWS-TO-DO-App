ca00e39f609557943bb763c8d97c653817d93508
Aditya Mudgerikar <aditya18thm@gmail.com>
2020-07-13 19:32:12 +0530

    Trial with new node version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { ROUTES } from './shared/const/routes.const';
 
+
 const routes: Routes = [
   {
     path: '',
